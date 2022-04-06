77a8ae1f86798a19c3f388567154946ddfd68306
AryaDuta10 <aryadutanusa2@gmail.com>
2022-04-07 02:57:44 +0700

    menyelesaikan tanggungan

---
+++ Jobsheet5/Tugas 5/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Main from './Tugas/Main';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* Untuk menjalankan Praktikum */}
+    {/* <App /> */}
+
+    {/* Untuk menjalankan Tugas */}
+    <Main/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ Jobsheet6/PBF/tugas_modul_03/src/container/BlogPost/BlogPost.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import './BlogPost.css';
+
+class BlogPost extends Component{
+    render(){
+        return(
+        <p>Blog Artikel</p>
+        )}
+}
+export default BlogPost;
